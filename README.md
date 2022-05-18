02b594966909f3963a4727bc3ad62748a43af08a
Olmeiro orozco <olme.orozco@gmail.com>
2022-05-18 11:45:45 -0500

    config apollo client main.jsx

---
+++ README.md
@@ -0,0 +1,12 @@
+# GraphQL React
+
+### execute 
+```
+ npm install
+```
+
+```
+ npm run dev
+```
+
+### Client used: https://www.apollographql.com/docs/react
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { ApolloClient, gql, HttpLink, InMemoryCache } from '@Apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+})
+
+const query = gql`
+query {
+  allPersons {
+    id
+    name
+    phone
+    address {
+      street
+      city
+    }
+  }
+}`
+
+//usamos la query
+client.query({query: query })
+  .then(res => {
+    console.log(res.data);
+  })
 
 ReactDOM.render(
   <React.StrictMode>
